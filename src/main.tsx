fdb26a094237d21750698c4f9e5d97333d6c0622
nguyenvandung10x7-eng <nguyenvandung10x7@gmail.com>
2026-08-24 11:59:01 +0700

    Apply exploration app visual system

--- src/main.tsx
+++ src/main.tsx
@@ -9,6 +9,7 @@ import './book-soundtrack.css';
 import './field-v2.css';
 import './typography-v2.css';
 import './surface-label-redesign.css';
+import './atlas-obscura-inspired.css';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
