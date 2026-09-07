0074e73738bb6cb4a0aa093ab2d2e5a85b5b9d7b
nguyenvandung10x7-eng <nguyenvandung10x7@gmail.com>
2026-09-07 10:32:53 +0700

    Remove obsolete dark FIELD stylesheet

--- src/main.tsx
+++ src/main.tsx
@@ -6,7 +6,6 @@ import { AuthProvider } from './contexts/AuthContext';
 import './i18n/reactI18next';
 import './index.css';
 import './book-soundtrack.css';
-import './field-v2.css';
 import './typography-v2.css';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
